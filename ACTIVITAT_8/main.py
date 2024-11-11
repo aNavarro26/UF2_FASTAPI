34e42f64766dd475f143ccf3092b812b32d52fdf
Adrian Navarro <2023_adrian.navarro@iticbcn.cat>
2024-11-11 19:05:19 +0100

    POST method done

--- ACTIVITAT_8/main.py
+++ ACTIVITAT_8/main.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-@app.get("/users/{users_id}")
+class User(BaseModel):
+    name: str
+    age: int
+    location: str
+    email: str
+    phone_number: str = None
+    manager_name: str = None
+
+
+@app.get("/users/{user_id}")
 async def read_user(user_id: int):
     return {"user_id": user_id}
+
+
+@app.post("/users/")
+async def create_user(user: User):
+    return {"user": user}
