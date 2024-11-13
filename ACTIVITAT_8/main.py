211e4f0a64fd1ef81558c06715e3fed97df7ffdd
Adrian Navarro <2023_adrian.navarro@iticbcn.cat>
2024-11-13 20:15:34 +0100

    HTTPException added

--- ACTIVITAT_8/main.py
+++ ACTIVITAT_8/main.py
@@ -1,23 +1,14 @@
-from fastapi import FastAPI, Response
-from pydantic import BaseModel
-from typing import Optional
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
-
-class User(BaseModel):
-    name: str
-    age: int
-    location: str
-    email: str
-    phone_number: Optional[str] = None
-    manager_name: Optional[str] = None
+# com si fos una BDD
+items = {"foo": "The Foo Wrestlers"}
 
 
-@app.get("/users/{user_id}")
-async def read_user(user_id: int, response: Response):
-    # com si el 42 no existeixi
-    if user_id == 42:
-        response.status_code = 404
-        return {"detail": "User not found"}
-    return {"user_id": user_id}
+@app.get("/items/{item_id}")
+async def read_item(item_id: str):
+    if item_id not in items:
+        # excepció per a quan no es troba
+        raise HTTPException(status_code=404, detail="Item not found")
+    return {"item": items[item_id]}
