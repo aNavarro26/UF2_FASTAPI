b4ca7e337efbc8f4b0e4510cc556a7a6af3047cf
Adrian Navarro <2023_adrian.navarro@iticbcn.cat>
2024-11-22 15:09:51 +0100

    new main created

--- ACTITIVAT_9/main.py
+++ ACTITIVAT_9/main.py
@@ -1,49 +1,14 @@
-from fastapi import FastAPI
-from db_connect import create_connection
-from pydantic import BaseModel
+from fastapi import FastAPI, HTTPException
 from typing import List
+from schemas.user_scheme import UserSchema
+from crud.crud import read_users
 
 app = FastAPI()
 
 
-class UserSchema(BaseModel):
-    user_id: int
-    user_name: str
-    user_surname: str
-    user_age: int
-    user_email: str
-
-
 @app.get("/users/", response_model=List[UserSchema])
-async def read_users():
-    connection = create_connection()
-    if connection:
-        try:
-            cursor = connection.cursor()
-
-            cursor.execute("SELECT * FROM users;")
-            results = cursor.fetchall()
-
-            cursor.close()
-            connection.close()
-
-            users = []
-
-            # per cada fila en el resultat de la consulta que agafa tots itero per afegir-ho a la llista
-            # que he fet per a després poder fer return d'aquesta que guardo cada resultat en forma d'objecte json
-            for row in results:
-                user = {
-                    "user_id": row[0],
-                    "user_name": row[1],
-                    "user_surname": row[2],
-                    "user_age": row[3],
-                    "user_email": row[4],
-                }
-
-                users.append(user)
-
-            return users
-        except Exception as e:
-            return {"status": "Error durant l'execució de la consulta", "error": str(e)}
-    else:
-        return {"status": "Connexió fallida"}
+async def get_users():
+    users = read_users()
+    if users is None:
+        raise HTTPException(status_code=500, detail="No es poden obtenir els usuaris.")
+    return users
